e265d76bb4cea19ddbcd962b366e9ecd8cbbb306
Andrew Hansen <sunlao93@gmail.com>
2026-07-27 07:46:57 -0400

    update

--- src/sw/enqueue.py
+++ src/sw/enqueue.py
@@ -5,6 +5,9 @@ from sw import broker, example
 async def main() -> None:
     await broker.startup()
     task = await example.kiq(1)
+    print(task, flush=True)
+    print(type(task), flush=True)
+    print(vars(task), flush=True)    
     print(f"enqueue: {task.task_id}")
     await broker.shutdown()
 
