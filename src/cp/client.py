3b94ea5eba07fad3758ba401f279b33e5653cb64
Andrew Hansen <sunlao93@gmail.com>
2026-07-30 00:45:43 -0400

    check updates

--- src/cp/client.py
+++ src/cp/client.py
@@ -1,18 +1,23 @@
-async def enqueue(self, request: EnqueueRequest) -> str | None:
-    if request.EnqueueGate:
-        raise RuntimeError("Enqueue Gate Closed")
+from taskiq.abc.broker import AsyncBroker
+from shared.models.worker import EnqueueRequest
 
-    job_id = request.Job.Config.Id
 
-    if await self.unique_job.exists(job_id):
-        return None
+class Client:
+    def __init__(self, broker: AsyncBroker) -> None:
+        self.broker = broker
 
-    task = await (
-        self.broker
-        .task_by_name(request.JobType)
-        .kicker()
-        .with_labels(job_id=job_id)
-        .kiq(request.Job)
-    )
+    async def startup(self) -> None:
+        await self.broker.startup()
 
-    return task.task_id
+    async def shutdown(self) -> None:
+        await self.broker.shutdown()
+
+    async def enqueue(self, request: EnqueueRequest):
+        if request.EnqueueGate:
+            raise RuntimeError("Enqueue Gate Closed")
+        task = self.broker.task_by_name(request.JobType)
+        return (
+            await task.kicker()
+            .with_labels(job_id=request.Job.Config.Id)
+            .kiq(request.Job)
+        )
