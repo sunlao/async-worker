ccc6d864afee02e7833f0e10a43bfd6bacc96f94
Andrew <sunlao93@gmail.com>
2026-08-08 22:14:51 -0400

    Update constants.py

--- src/shared/models/constants.py
+++ src/shared/models/constants.py
@@ -20,6 +20,7 @@ class ActionTypes(StrEnum):
     SUBPROCESS = "subprocess"  # issue subprocces command
     GET = "get"  # GET response from API
     POST = "post"
+    SET = "set" # redis kvp set
 
 
 class Audit(NamedTuple):
