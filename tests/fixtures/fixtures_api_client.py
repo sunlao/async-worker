dd98cedf63cac15bb89696c320c1ab783d1a62f4
Andrew Hansen <sunlao93@gmail.com>
2026-08-09 10:36:42 -0400

    fix

--- tests/fixtures/fixtures_api_client.py
+++ tests/fixtures/fixtures_api_client.py
@@ -4,6 +4,6 @@ from httpx import AsyncClient
 
 @fixture
 async def awork_api_client():
-    test_api = "http://awork-api:80/api/v1/"
+    test_api = "http://awork-worker:80/api/v1/"
     async with AsyncClient(base_url=test_api, timeout=10) as client:
         yield client
