cfe7cbeab849afff0a0217c0fe26e93c10fb0306
Andrew <sunlao93@gmail.com>
2026-08-08 23:25:51 -0400

    Update router.py

--- src/worker/core/router.py
+++ src/worker/core/router.py
@@ -1,15 +1,25 @@
 from taskiq import AsyncBroker, TaskiqDepends, TaskiqState
 from shared.models.constants import JobTypes
-from shared.models.worker import HelloConfig, JobConfig
+from shared.models.worker import AdminConfig, HelloConfig, JobConfig
+from worker.handler.admin import Admin
 from worker.handler.hello import Hello
 
 
+async def admin(
+    config: JobConfig[AdminConfig],
+    context: TaskiqState = TaskiqDepends(),
+) -> None:
+    await Admin(context).handle(config)
+
+
 async def hello(
-    config: JobConfig[HelloConfig], context: TaskiqState = TaskiqDepends()
+    config: JobConfig[HelloConfig],
+    context: TaskiqState = TaskiqDepends(),
 ) -> None:
     await Hello(context).handle(config)
 
 
 def register(queue: AsyncBroker) -> AsyncBroker:
+    queue.task(task_name=JobTypes.ADMIN)(admin)
     queue.task(task_name=JobTypes.HELLO)(hello)
     return queue
